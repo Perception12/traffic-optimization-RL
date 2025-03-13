2b080ed42f6b29726fbcdc076e9626f59751b532
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2025-03-14 00:14:18 +0100

    update config file for each models

--- fixed_time/config.py
+++ fixed_time/config.py
@@ -1,8 +1,8 @@
 class Config:
     num_episodes = 50
     max_steps = 1000
-    output_paths = ["results/uniform_traffic_train_data.csv", "results/heavy_traffic_NS_train_data.csv", "results/heavy_traffic_WE_train_data.csv"]
-    test_output_paths = ["results/uniform_traffic_test_data.csv", "results/heavy_traffic_NS_test_data.csv", "results/heavy_traffic_WE_test_data.csv"]
+    output_paths = ["results/ft_uniform_traffic_train_data.csv", "results/ft_heavy_traffic_NS_train_data.csv", "results/ft_heavy_traffic_WE_train_data.csv"]
+    test_output_paths = ["results/ft_uniform_traffic_test_data.csv", "results/ft_heavy_traffic_NS_test_data.csv", "results/ft_heavy_traffic_WE_test_data.csv"]
     scenario_names = ["uniform_traffic", "heavy_traffic_NS", "heavy_traffic_WE"]
     phase_durations = [[10, 10, 10, 10], [10, 10, 10, 20], [10, 20, 10, 10]]
 config = Config()
--- sarsa/config.py
+++ sarsa/config.py
@@ -2,8 +2,8 @@ class Config:
     num_episodes = 50
     max_steps = 1000
     learning_rate = 0.001
-    output_paths = ["results/uniform_traffic_train_data.csv", "results/heavy_traffic_NS_train_data.csv", "results/heavy_traffic_WE_train_data.csv"]
-    test_output_paths = ["results/uniform_traffic_test_data.csv", "results/heavy_traffic_NS_test_data.csv", "results/heavy_traffic_WE_test_data.csv"]
+    output_paths = ["results/sarsa_uniform_traffic_train_data.csv", "results/sarsa_heavy_traffic_NS_train_data.csv", "results/sarsa_heavy_traffic_WE_train_data.csv"]
+    test_output_paths = ["results/sarsa_uniform_traffic_test_data.csv", "results/sarsa_heavy_traffic_NS_test_data.csv", "results/sarsa_heavy_traffic_WE_test_data.csv"]
     scenario_names = ["uniform_traffic", "heavy_traffic_NS", "heavy_traffic_WE"]
 
 config = Config()
